abef89d6be99b2c681d087907dedfb4df417baa5
Ubuntu <sachinchandra.999@gmail.com>
2017-04-13 23:05:42 +0000

    table if not exists

--- createTables.py
+++ createTables.py
@@ -5,10 +5,10 @@ conn = psycopg2.connect(database="example", user='oorjan',
 print("Opened database successfully")
 
 cur = conn.cursor()
-cur.execute('create table referencedata(latitude decimal, longitude decimal, systemcapacity decimal, cityname varchar(30),cityid integer, primary key (cityid));')
-cur.execute('create table data(location Integer not null, city varchar(30) , solarid Integer not null, dc decimal , timestamp timestamp );')
+cur.execute('create table if not exists referencedata(latitude decimal, longitude decimal, systemcapacity decimal, cityname varchar(30),cityid integer, primary key (cityid));')
+cur.execute('create table if not exists data(location Integer not null, city varchar(30) , solarid Integer not null, dc decimal , timestamp timestamp );')
 cur.execute(
-    'create table  emailid( solarid Integer not null, emailid varchar(30) , primary key (solarid));')
+    'create table if not exists emailid( solarid Integer not null, emailid varchar(30) , primary key (solarid));')
 print "Table created successfully"
 
 conn.commit()
