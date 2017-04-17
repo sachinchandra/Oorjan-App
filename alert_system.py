7c08c032f9af981d10bd19672cde287c312d7563
sachin chandra <sachinchandra.999@gmail.com>
2017-04-17 13:01:07 +0530

    changed to take constants from conf file

--- alert_system.py
+++ alert_system.py
@@ -5,11 +5,14 @@ import psycopg2
 from collections import OrderedDict
 import smtplib
 from email.mime.text import MIMEText
+from conf import db_name, db_port, db_host, db_user, db_password,api_host, api_port
+
 import sys
+
 sys.path.insert(0, '/var/www/html/flaskapp')
 
-conn = psycopg2.connect(database="example", user='oorjan',
-                        password='oorjan', host="127.0.0.1", port="5432")
+conn = psycopg2.connect(database=db_name, host=db_host,
+                        port=db_port, user=db_user, password=db_password)
 
 cur = conn.cursor()
 
@@ -29,7 +32,7 @@ params = OrderedDict([('date', newformat)])
 # Make request and get statuses for mailing
 finalEmails = []
 for solarid in solaremailids:
-    r = requests.get("http://localhost/{}".format(solarid[0]), params=params)
+    r = requests.get("http://{host}:{port}/{path}".format(host=api_host, port=api_port,path = solarid[0]), params=params)
     finalEmails.append({'Subject': "underperforming hours of your solar panel",
                         'Message': r.text,
                         'To': solarid[1]})
